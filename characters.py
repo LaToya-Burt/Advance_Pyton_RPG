ad428e0ce1a44b41aab5c88ab0eaf38d585d85ef
LaToya <latoya.y.burt@gmail.comgit config --global user.name LaToyagit push$ git config --global user.emaillatoya.y.burt@gmail.com$ git config --global user.name LaToya-Burtgit add.$ git add.git config --global user.email latoya.y.burt@gmail.com>
2022-11-01 23:09:49 -0400

    Added new file characters and created my heros and enemys

---
+++ characters.py
@@ -0,0 +1,59 @@
+
+
+the_hero={
+"name": "Mom", 
+"level": 38, 
+"health": 100,
+"equipment": {"purse", "shoe", "van"},
+"attacks": (("saying no",100), ("shoe attack", 70), ("laundry", 55)),
+"coins":{
+    "copper": 150,
+    "sliver": 80,
+    "gold": 20,
+}
+
+}
+
+enemy_one={
+"name": "Jaelen",
+"level": 18,
+"health": 100,
+"equipment": {"Xbox", "hoodies", "permit"},
+"attacks": (( "closed doors", 50), ("smart mouth", 70), ("bear hug", 50)),
+"coins":{
+    "copper": 100,
+    "silver": 20,
+    "gold": 40,
+
+}
+
+}
+
+enemy_two={
+"name": "Jaida",
+"level": 16,
+"health": 100,
+"equipment":{"cellphone", "bookbags", "leggings"},
+"attacks": (("eye roll",70 ), ("slamming doors", 50),( "needing a ride", 80)),
+"coins": {
+    "copper":0,
+    "silver": 0,
+    "gold": 0,
+}
+
+}
+
+enemy_three= {
+"name": "Junious",
+"level": 14,
+"health":100,
+"equipment": {"switch", "skinny jeans", "headphones"},
+"attacks": (("saying huh", 50), ("attitude", 70), ("big smile", 100)),
+"coins":{
+    "copper":50,
+    "silver":80,
+    "gold":100,
+    
+}    
+
+}
